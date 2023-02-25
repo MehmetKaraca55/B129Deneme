e41e4847c12f748062d025f832d87a8f6acb31b0
Mehmet Karaca <mehmetkaraca55@gmail.com>
2023-02-25 18:52:15 +0300

    v4

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -1,5 +1,7 @@
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("Dünya");
     }
-}
+}//Restore--> Tamamen working space'ı temizler
+//Stash--> Working Space'ı hafızaya alır.
